146e9bbe04c4452365c15ef1183505760d0e4f85
Kenneth <kennethyoung.2960@gmail.com>
2024-03-21 19:48:39 -0500

    2 of 2 tests passing

--- Making_requests_Requests_in_JavaScript_Node_18_1/src/main.js
+++ Making_requests_Requests_in_JavaScript_Node_18_1/src/main.js
@@ -1,7 +1,19 @@
 const axios = require("../utils/axios");
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5000"; //add this into the functions as the variable
 
-function getAllNames() {}
+function getAllNames() {
+  return axios.get(`${BASE_URL}/constellations`)
+    .then(response => {
+      const constellations = response.data;
+      const constellationNames = constellations.map(constellations => constellations.name);
+      console.log(constellationNames);
+      return constellationNames;
+    })
+    .catch(error => {
+      console.error('Error fetching constellation names:', error);
+      throw error;
+    })
+}
 
 function getConstellationsByQuadrant(quadrant) {}
 
